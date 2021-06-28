56d3f95bc61a0b7daff99be20a0f66d3eee5aea9
omer faruk aydin <oemer.aydin@powercoders.org>
2021-06-28 16:59:18 +0200

    .

---
+++ 06-Shop/front/src/components/Product.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export function Product(){
+    return <main><h1>Title</h1></main>
+
+}
+
--- 06-Shop/front/src/main.tsx
+++ 06-Shop/front/src/main.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { HashRouter, Switch, Route, Link } from "react-router-dom";
 import * as ReactDOM from "react-dom";
+import {ProductList} from "./components/ProductList";
 
 const appDiv = document.getElementById("app");
 
@@ -8,7 +9,13 @@ if (!(appDiv instanceof HTMLDivElement)) {
 	throw new Error("No div with id 'app' found");
 }
 
-// TODO: Write your React app ;)
-const App = () => <p>Hello World</p>;
+const App = () => <HashRouter>
+	<Switch> 
+	<Route path="/product">Products</Route>
+	<Route path="/"><ProductList/></Route>
+
+	
+	</Switch>
+</HashRouter>;
 
 ReactDOM.render(<App />, appDiv);
